f94c976073b68ea6933dd3adb75ddd82a71bd24a
noobeamm <noobeamm@hotmail.com>
2014-09-13 05:52:51 +0700

    Bello

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1338;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Azure\n Bello');
+  res.end('Hello Azure Rock \n This is Node.js');
 }).listen(port);
